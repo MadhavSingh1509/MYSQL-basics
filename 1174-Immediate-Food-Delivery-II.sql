18e35c669ecce35e34ce0633eabd9cac5e042a16
Damon Salvatore <165358433+MadhavSingh1509@users.noreply.github.com>
2026-08-06 09:57:04 +0530

    [1174] [Time Beats: 55.67%] - LeetPush

---
+++ 1174-Immediate-Food-Delivery-II.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+ROUND(
+    AVG(order_date = customer_pref_delivery_date) * 100,
+    2
+) AS immediate_percentage
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id
+               ORDER BY order_date
+           ) AS rn
+    FROM Delivery
+) t
+WHERE t.rn = 1;
